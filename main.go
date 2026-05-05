50ae6968fe3e8dc1219ec2547970f57db20e3041
Vetle Kise <96139038+vetlekise@users.noreply.github.com>
2026-05-05 10:02:02 +0200

    feat: implement client for fetching todos

--- main.go
+++ main.go
@@ -1,2 +1,31 @@
 // Package main is the entry point for the go-rest-api tool.
 package main
+
+import (
+	"context"
+	"flag"
+	"log/slog"
+	"net/http"
+	"time"
+
+	"github.com/vetlekise/go-rest-api/client"
+)
+
+var url string
+var httpClient = &http.Client{Timeout: 10 * time.Second}
+
+func init() {
+	flag.StringVar(&url, "URL", "https://jsonplaceholder.typicode.com/", "The URL to test CRUD operations.")
+	flag.Parse()
+}
+
+func main() {
+	c := client.New(url, httpClient)
+
+	todo, err := c.GetTodo(context.Background())
+	if err != nil {
+		slog.Error("get request failed", "err", err)
+		return
+	}
+	slog.Info("todo", "id", todo.ID, "title", todo.Title, "completed", todo.Completed)
+}
